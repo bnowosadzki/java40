87998ed7eca5f3f822bfef1d0db49d0335e92085
bnowosadzki <163306948+bnowosadzki@users.noreply.github.com>
2024-05-08 16:12:40 +0000

    4_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,52 @@
-/*
-Kod bazowy programu Commit4_0: 
-• Program dodaje do prostej bazy danych (pliku db.txt) dane odnośnie Studentów.
-• Studenci dodawani są w klasie Main.
-• Wszyscy studenci są wypisywani na końcu klasy Main.
-• Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
-• Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
-*/
-
+import java.util.Scanner;
 import java.io.IOException;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner scanner = new Scanner(System.in);
+
+      while (true) {
+
+        System.out.println("1. Dodaj studenta");
+        System.out.println("2. Wyświetlwszystkich studentów");
+        System.out.println("3. Wyjdz");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine(); 
+        switch (choice) {
+          case 1:
+            System.out.println("Podaj imię studenta:");
+            String name = scanner.nextLine();
+            System.out.println("Podaj nazwisko studenta:");
+            String nazwisko = scanner.nextLine();
+            System.out.println("Podaj wiek studenta:");
+            int wiek = scanner.nextInt();
+            scanner.nextLine(); 
+            s.addStudent(new Student(name,nazwisko,wiek));
+            break;
+            case 2:
+            var students = s.getStudents();
+            for (Student current : students) {
+              System.out.println(current.ToString());
+            }
+            break;
+
+          case 3:
+            System.exit(0);
+            break;
+            default:
+            System.out.println("Niepoprawny wybór.");
+            continue;
+        }
+        System.out.println("Czy chcesz dodać kolejnego studenta? (T/N)");
+        String answer = scanner.nextLine();
+        if (!answer.equalsIgnoreCase("T")) {
+          break;
+        }
+      }
+
 
       var students = s.getStudents();
       for(Student current : students) {
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,24 +1,28 @@
 public class Student {
 
-  private String Name;
-  private int Age;
+private String Name;
+private int Age;
+private String Nazwisko;
 
-  public Student(String name, int age) {
-    Name = name;
-    Age = age;
-  }
 
-  public String GetName() {return Name;}
-  public int GetAge() {return Age;}
+public Student(String name, String nazwisko, int age) {
+  Name = name;
+  Age = age;
+  Nazwisko = nazwisko;
+}
 
-  public String ToString() {
-    return Name + " " + Integer.toString(Age);
-  }
+public String GetName() {return Name;}
+public int GetAge() {return Age;}
+public String GetNazwisko() {return Nazwisko;}
 
-  public static Student Parse(String str) {
+public String ToString() {
+  return Name + " " + Nazwisko + " " + Integer.toString(Age);
+}
+
+public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 2) 
-      return new Student("Parse Error", -1);
-    return new Student(data[0], Integer.parseInt(data[1]));
+    if(data.length != 3) 
+      return new Student("Parse Error","Parse Error", -1);
+    return new Student(data[0],data[1], Integer.parseInt(data[2]));
   }
 }
