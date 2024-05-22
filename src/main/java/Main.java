497da517c4aea2186c931bcf23312abeb6ef96bb
bnowosadzki <163306948+bnowosadzki@users.noreply.github.com>
2024-05-22 12:38:21 +0000

    4_5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 class Main {
   public static void main(String[] args) {
@@ -10,11 +13,11 @@ class Main {
       while (true) {
 
         System.out.println("1. Dodaj studenta");
-        System.out.println("2. Wyświetlwszystkich studentów");
+        System.out.println("2. Wyświetl wszystkich studentów");
         System.out.println("3. Wyjdz");
 
         int choice = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine();
         switch (choice) {
           case 1:
             System.out.println("Podaj imię studenta:");
@@ -23,20 +26,26 @@ class Main {
             String nazwisko = scanner.nextLine();
             System.out.println("Podaj wiek studenta:");
             int wiek = scanner.nextInt();
-            scanner.nextLine(); 
-            s.addStudent(new Student(name,nazwisko,wiek));
-            break;
-            case 2:
-            var students = s.getStudents();
-            for (Student current : students) {
-              System.out.println(current.ToString());
+            scanner.nextLine();
+            System.out.println("Podaj datę urodzenia studenta (yyyy-MM-dd):");
+            String dateOfBirth = scanner.nextLine();
+
+            if (isValidDate(dateOfBirth)) {
+              s.addStudent(new Student(name, nazwisko, wiek, dateOfBirth));
+            } else {
+              System.out.println("Niepoprawny format daty. Użyj formatu yyyy-MM-dd.");
             }
             break;
-
+          case 2:
+            var students = s.getStudents();
+            for (Student current : students) {
+              System.out.println(current.toString());
+            }
+            break;
           case 3:
             System.exit(0);
             break;
-            default:
+          default:
             System.out.println("Niepoprawny wybór.");
             continue;
         }
@@ -47,13 +56,23 @@ class Main {
         }
       }
 
-
       var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+      for (Student current : students) {
+        System.out.println(current.toString());
       }
     } catch (IOException e) {
+      System.out.println("Wystąpił błąd I/O: " + e.getMessage());
+    }
+  }
 
+  private static boolean isValidDate(String date) {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    sdf.setLenient(false);
+    try {
+      Date parsedDate = sdf.parse(date);
+      return true;
+    } catch (ParseException e) {
+      return false;
     }
   }
 }
